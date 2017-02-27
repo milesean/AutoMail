b7b4cc5e4027fa43a1d00e04a19ba77797c85b71
Mikael <mikael@Mikaels-iMac.local>
2017-02-27 15:47:56 -0700

    2/27/17

--- automail.py
+++ automail.py
@@ -8,11 +8,13 @@ import csv
 
 from bs4 import BeautifulSoup
 
+from time import sleep
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-username = raw_input('Enter username: ')  # asking for username for gmail server
-password = raw_input('Enter password: ')  # password for gmail account (app password)
+username = raw_input('Enter username: ')  # asking for username for Gmail server
+password = raw_input('Enter password: ')  # password for Gmail account (app password)
 
 smtpObj = smtplib.SMTP('smtp.gmail.com', 587)  # Setting smtp object with smtp server
 
@@ -22,33 +24,40 @@ smtpObj.starttls()  # TLS Encryption
 
 smtpObj.login(username, password) # Login to SMTP server
 
+HEADERS = ['Name', 'Email', 'Company'] # Header row for csv file
+
 # opens csv file and reads each row
-with open('emails.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+with open('emails.csv', 'rU') as csvfile:
+    reader = csv.DictReader(csvfile, HEADERS)
     # reading each row
     for row in reader:
-        l = row[0].split(',')  # each row is read as one list item, splits into multiple list items delimited by comma
 
-        name = l[0]  # sets name from first list item
-
-        recipient = l[1]  # sets email address from second list item
+        if not row['Name'] or not row['Email'] or not row['Company']:
+            continue
 
         soup = BeautifulSoup(open("content.html"), 'html.parser')  # parses HTML file and creates soup obj
 
         message = MIMEMultipart('alternative')  # creates message
 
-        message['Subject'] = "Test"  # message subject line
+        message['Subject'] = "Please Forward This Email to Mikael"  # message subject line
 
-        content = str(soup).format(name)  # soup obj into string
+        html = str(soup)  # soup obj into string
+
+        content = html.format(**row)
 
         email = MIMEText(content, 'html')  # creates email
 
         message.attach(email)  # combines email and message components
 
+        recipient = row['Email']  # Sets email address for recipient
+
         smtpObj.sendmail(username, recipient, message.as_string())  # Sending email
 
-smtpObj.quit()  # Disconnect from server
+        print(recipient)  # Prints each recipient to the console
 
+        sleep(3)  # Time delay in seconds after each iteration
+
+smtpObj.quit()  # Disconnect from server
 
 
 
